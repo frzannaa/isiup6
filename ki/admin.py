437555420a5bbe8d999e4774274a1c93c322ef95
Farzana Afifah Razak <81352414+frzannaa@users.noreply.github.com>
2023-11-25 13:52:58 +0700

    Delete ki directory

--- ki/admin.py
+++
@@ -1,9 +0,0 @@
-# ki/admin.py
-
-from django.contrib import admin
-from .models import User, PersonalInfo, MedicalInfo, BiometricData
-
-admin.site.register(User)
-admin.site.register(PersonalInfo)
-admin.site.register(MedicalInfo)
-admin.site.register(BiometricData)
